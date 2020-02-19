cdd72b23fa36f3b6ec4cb28d5101a9b0dcce5941
pnirtep <pnirtep@gmail.com>
2020-02-19 21:35:43 +0300

    api views add

--- courses_api/serializers.py
+++ courses_api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
 
 from courses.models import Course, Teacher, Lesson, Student
 
@@ -11,24 +12,30 @@ class TeacherSerializer(serializers.HyperlinkedModelSerializer):
         model = Teacher
         fields = 'url', 'id', 'first_name', 'teacher_courses',
 
+
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
     course_teacher = TeacherSerializer(read_only=True, many=True)
+
     class Meta:
         model = Course
         fields = 'id', 'title', 'price', 'start_date', 'course_teacher'
+
 
 class LessonSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Lesson
         fields = 'id', 'title', 'description', 'course'
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = 'id', 'username', 'email'
 
+
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
     user = UserSerializer(read_only=True)
+    accessed_courses = serializers.SlugRelatedField(many=True, read_only=True, slug_field='title')
     class Meta:
         model = Student
         fields = 'user', 'bio', 'location', 'accessed_courses'
