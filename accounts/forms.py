f6d9a136e9d63fb278970e1f8ad735ffb8855d4a
pnirtep <pnirtep@gmail.com>
2020-02-19 18:07:58 +0300

    reg form add

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,35 +1,26 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
-
 from courses.models import Student
 
 
-class SignUpForm(forms.ModelForm):
+class BaseForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
-    class Meta:
-        model = User
-        fields = [
-            'username',
-            'password',
 
-                        ]
 
-class StudentProfileForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
-    class Meta:
-        model = Student
-        fields = ('bio','location')
+class SignUpForm(BaseForm):
+    username = forms.CharField(widget=forms.TextInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.CharField(widget=forms.TextInput)
+    bio = forms.CharField(widget=forms.Textarea)
+    location = forms.CharField(widget=forms.TextInput)
+
 
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
-
--- accounts/views.py
+++ accounts/views.py
@@ -1,47 +1,10 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.views.generic import CreateView
 from django.views.generic.base import View
 from django.contrib.auth import authenticate, login, logout
-from accounts.forms import StudentProfileForm, SignUpForm, LoginForm
+from accounts.forms import LoginForm, SignUpForm
 
-
-# class RegisterUserView(View):
-#     user_form_class = SignUpForm
-#     student_form_class = StudentProfileForm
-#     template_name = 'accounts/register_form.html'
-#
-#     def get(self, request):
-#         user_form = self.user_form_class
-#         student_form = self.student_form_class
-#         return render(request, self.template_name, {'user_form': user_form, 'student_form': student_form})
-#
-#     def post(self, request):
-#         user_form = SignUpForm(request.POST)
-#         student_form = StudentProfileForm(request.POST)
-#         if user_form.is_valid() and student_form.is_valid():
-#             user_form.save()
-#             student_form.save()
-#             return render(request, 'courses/index.html')
-
-# def register_user(request):
-#     if request.method == 'POST':
-#         user_form = SignUpForm(request.POST, instance=request.user)
-#         student_form = StudentProfileForm(request.POST, instance=request.user)
-#         if user_form.is_valid() and student_form.is_valid():
-#             user_form.save()
-#             student_form.save()
-#             return redirect('courses')
-#         else:
-#             context = {
-#                 'user_form': user_form,
-#                 'student_form': student_form,
-#             }
-#     else:
-#         context = {
-#             'user_form': SignUpForm(),
-#             'student_form': StudentProfileForm(),
-#         }
-#     return render(request, 'accounts/register_form.html', context)
 
 def login_view(request):
     if request.method == 'GET':
@@ -69,10 +32,9 @@ def logout_view(request):
     return redirect('courses')
 
 
-
 def register_user(request):
+    form = SignUpForm
     if request.method == 'POST':
-
         username = request.POST.get('username')
         bio = request.POST.get('bio')
         email = request.POST.get('email')
@@ -86,5 +48,5 @@ def register_user(request):
         user.save()
         login(request, user)
         return redirect('courses')
-    else:
-        return render(request, 'accounts/register_form.html')
+
+    return render(request, 'accounts/register_form.html', {'form':form})
